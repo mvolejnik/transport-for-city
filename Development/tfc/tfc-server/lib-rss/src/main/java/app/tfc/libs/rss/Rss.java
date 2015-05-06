c351e35a8f5b236483fc9f23716cfeb6a3e7b802
Michal Volejnik <mvolejnik@users.noreply.github.com>
2015-05-06 07:29:26 +0200

    #3 RSS 2.0 support

---
+++ Development/tfc/tfc-server/lib-rss/src/main/java/app/tfc/libs/rss/Rss.java
@@ -0,0 +1,7 @@
+package app.tfc.libs.rss;
+
+public interface Rss {
+	
+	//TODO
+	
+}
---
+++ Development/tfc/tfc-server/lib-rss/src/main/java/app/tfc/libs/rss/impl/Rss20Impl.java
@@ -0,0 +1,26 @@
+package app.tfc.libs.rss.impl;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import app.tfc.libs.rss.RssException;
+import app.tfc.libs.rss.jaxb.rss20.Rss;
+
+public class Rss20Impl implements app.tfc.libs.rss.Rss{
+	
+	private Rss rss;
+
+	public Rss20Impl(InputStream rssInputStream) throws RssException{
+		try {
+	        JAXBContext ctx =  JAXBContext.newInstance(Rss.class);
+	        Unmarshaller mrs = ctx.createUnmarshaller();
+	        rss = (Rss) mrs.unmarshal(rssInputStream);
+	        
+        } catch (JAXBException e) {
+	        throw new RssException(e);
+        }
+	}
+}
