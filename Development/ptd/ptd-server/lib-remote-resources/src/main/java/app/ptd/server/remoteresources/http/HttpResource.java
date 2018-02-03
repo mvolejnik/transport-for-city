7ede34975b563a73a550963cdae06db04bc84c1b
Michal Volejnik <Michal.Volejnik@gmail.com>
2018-02-03 17:21:38 +0100

    #20 remote resources download

--- Development/ptd/ptd-server/lib-remote-resources/src/main/java/app/ptd/server/remoteresources/http/HttpResource.java
+++ Development/ptd/ptd-server/lib-remote-resources/src/main/java/app/ptd/server/remoteresources/http/HttpResource.java
@@ -18,22 +18,22 @@ import app.ptd.server.remoteresources.Resource;
 import app.ptd.server.remoteresources.ResourceImpl;
 
 public class HttpResource implements AutoCloseable {
-  
+
   private static final Logger l = LogManager.getLogger(HttpResource.class);
-  
+
   private static final String ETAG_IF_NONE_MATCH = "If-None-Match";
 
   CloseableHttpClient httpclient;
-  
+
   public HttpResource() {
     httpclient = HttpClients.createDefault();
   }
 
-  public Optional<Resource> content(URL resourceUrl) throws RemoteResourceException{
+  public Optional<Resource> content(URL resourceUrl) throws RemoteResourceException {
     return content(resourceUrl, null);
   }
-  
-  public Optional<Resource> content(URL resourceUrl, String etag) throws RemoteResourceException{
+
+  public Optional<Resource> content(URL resourceUrl, String etag) throws RemoteResourceException {
     try {
       Optional<Resource> resource;
       HttpGet httpGet;
@@ -42,23 +42,53 @@ public class HttpResource implements AutoCloseable {
         httpGet.addHeader(ETAG_IF_NONE_MATCH, etag);
       }
       CloseableHttpResponse response = httpclient.execute(httpGet);
-      if (response.getStatusLine().getStatusCode() == 403 || response.getEntity() == null) {
-        resource = Optional.empty();
-      } else {
-      HttpEntity entity = response.getEntity();
+      ResponseStatusCode statusCode = new ResponseStatusCode(response.getStatusLine().getStatusCode());
+      if (statusCode.isOk()) {
+        HttpEntity entity = response.getEntity();
         resource = Optional.ofNullable(new ResourceImpl(entity.getContent()));
+      } else if (statusCode.isClientError()) {
+        l.error("Unable to get resource '{}' due to client error: '{}' ({}).", resourceUrl, statusCode.statusCode,
+            response.getStatusLine().getReasonPhrase());
+        throw new RemoteResourceException(String.format("Unable to get resource '%s' due to client error: '%s' (%s).",
+            resourceUrl, statusCode.statusCode, response.getStatusLine().getReasonPhrase()));
+      } else {
+        l.warn("Resource '{}' unexpected response '{}' ({}).", resourceUrl, statusCode.statusCode,
+            response.getStatusLine().getReasonPhrase());
+        HttpEntity entity = response.getEntity();
+        resource = entity == null ? Optional.empty() : Optional.ofNullable(new ResourceImpl(entity.getContent()));
       }
+
       return resource;
     } catch (URISyntaxException | IOException e) {
       l.error("content::", e);
       throw new RemoteResourceException(e);
-    }    
-    
+    }
+
   }
 
   @Override
   public void close() throws IOException {
     httpclient.close();
   }
-  
+
+  private static class ResponseStatusCode {
+    int statusCode;
+
+    ResponseStatusCode(int statusCode) {
+      if (statusCode < 100 || statusCode > 599) {
+        throw new IllegalArgumentException("Illegal HTTP Status code, expected range: 100 - 599.");
+      }
+      this.statusCode = statusCode;
+    }
+
+    public boolean isClientError() {
+      return statusCode >= 400 && statusCode <= 499;
+    }
+
+    public boolean isOk() {
+      return statusCode == 200 || statusCode == 304;
+    }
+
+  }
+
 }
--- Development/ptd/ptd-server/lib-remote-resources/src/test/java/app/ptd/server/remoteresources/http/HttpResourcesTest.java
+++ Development/ptd/ptd-server/lib-remote-resources/src/test/java/app/ptd/server/remoteresources/http/HttpResourcesTest.java
@@ -1,13 +1,24 @@
 package app.ptd.server.remoteresources.http;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import app.ptd.server.remoteresources.RemoteResourceException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +27,9 @@ public class HttpResourcesTest {
   private static Server server;
 
   private static final int PORT = 8088;
+  
+  private static final String NOT_MODIFIED = "NOT_MODIFY" ;
+  private static final String NOT_EXISTING_RESOURCE = "/not-existing-resource" ;
 
   @BeforeAll
   static void initAll() throws Exception {
@@ -26,8 +40,18 @@ public class HttpResourcesTest {
       webAppCtx.setContextPath("/test");
       webAppCtx.setResourceBase("src/test/webapp");
       webAppCtx.setClassLoader(HttpResourcesTest.class.getClassLoader());
-      server.setHandler(webAppCtx);
-
+      Handler notModifyHandler = new AbstractHandler() {
+        @Override
+        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+          if (NOT_MODIFIED.equals(request.getHeader("If-None-Match"))) {
+            response.setStatus(304);
+            baseRequest.setHandled(true);
+          }
+        }
+      };
+      HandlerList handlers = new HandlerList(notModifyHandler, webAppCtx);
+      server.setHandler(handlers);
       server.start();
     } catch (Exception e) {
       e.printStackTrace();
@@ -44,10 +68,32 @@ public class HttpResourcesTest {
   public void testRemoteResource() throws Exception {
     initAll();
     try (HttpResource httpResource = new HttpResource();
-        InputStream is = httpResource.content(new URL("http://localhost:" + PORT + "/test/simple.json")).get().content()
-        ) {
+        InputStream is = httpResource.content(new URL("http://localhost:" + PORT + "/test/simple.json"))
+            .get()
+            .content()) {
       byte[] b = new byte[17];
-      is.read(b);      assertEquals("{\"test\": \"test\"}", new String(b).substring(0, 16), "Unexpected Remote Resource Content.");
+      is.read(b);
+      assertEquals("{\"test\": \"test\"}", new String(b).substring(0, 16), "Unexpected Remote Resource Content.");
+    } finally {
+      tearDownAll();
+    }
+  }
+
+  @Test
+  public void testnotModify() throws Exception {
+    initAll();
+    try (HttpResource httpResource = new HttpResource();){
+        assertFalse(httpResource.content(new URL("http://localhost:" + PORT + "/test/simple.json"), NOT_MODIFIED).isPresent(), "Unmodified resource shouldn't be returned.");
+    } finally {
+      tearDownAll();
+    }
+  }
+  
+  @Test
+  public void testNotExistingResource() throws Exception {
+    initAll();
+    try (HttpResource httpResource = new HttpResource();){
+        assertThrows(RemoteResourceException.class, () -> httpResource.content(new URL("http://localhost:" + PORT + NOT_EXISTING_RESOURCE)));
     } finally {
       tearDownAll();
     }
